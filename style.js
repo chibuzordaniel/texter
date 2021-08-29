2d371a16131426db16659c605232341a16c63f17
chibuzordaniel <starchrischibuzor@gmail.com>
2021-08-29 20:28:04 +1300

    first commit

---
+++ style.js
@@ -0,0 +1,11 @@
+let dp = document.getElementsByClassName('dp');
+
+console.log( poro);
+// Change the CSS
+
+// dp.style.background = '#ffff'
+
+ 
+// let name =[1, 2, 3, 4, 5, 10, 8, 9]
+
+// console.log( name );
